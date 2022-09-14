dcbfc5a33f5e196da164af52ea2e742183ab866c
somehobo <nolan@ferriel.com>
2022-09-13 22:19:01 -0700

    string resource

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:song_snippet/resources/dimen.dart';
+import 'package:song_snippet/resources/strings.dart';
 
 
 class Home extends StatefulWidget {
@@ -22,7 +23,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Nusic"),
+        title: Text(SongSnippetStrings.title),
       ),
       body: ListView.builder(
         itemCount: songs.length,
