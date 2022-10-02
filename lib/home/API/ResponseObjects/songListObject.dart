845872c9f6052c8791f62714cae4f04b87598969
somehobo <nolan@ferriel.com>
2022-10-02 14:10:25 -0700

    added start and end fields

--- lib/home/API/ResponseObjects/songListObject.dart
+++ lib/home/API/ResponseObjects/songListObject.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
+
 class SongList {
   static const keySongList = "songList";
 
@@ -22,19 +24,29 @@ class SongList {
 class SongObject {
   static const keyName = "name";
   static const keyArtist = "artist";
+  static const keyStart = "start";
+  static const keyEnd = "end";
   static const keySong = "song";
 
   final String name;
   final String artist;
+  final int start;
+  final int end;
   final String song;
 
   const SongObject({
     required this.name,
     required this.artist,
+    required this.start,
+    required this.end,
     required this.song
   });
 
   factory SongObject.fromJson(Map<String, dynamic> json) {
-    return SongObject(name: json[keyName], artist: json[keyArtist], song: json[keySong]);
+    return SongObject(name: json[keyName],
+        artist: json[keyArtist],
+        start: json[keyStart],
+        end: json[keyEnd],
+        song: json[keySong]);
   }
 }
