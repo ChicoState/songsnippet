e31deaab56de8c555f0e5042d9e546f4727001dd
somehobo <nolan@ferriel.com>
2022-10-02 14:20:57 -0700

    added more fields to the backend

--- Project/Nusic/myapi/serializers.py
+++ Project/Nusic/myapi/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
-from .models import Song
 from .models import SongModel
 
-
-class SongSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model=Song
-        fields=('name', 'artist')
 
 class SongModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=SongModel
-        fields=('name', 'artist', 'song')
+        fields=('name', 'artist','start', 'end', 'song')
