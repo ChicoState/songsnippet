9a1a291e9ebd21445ff4a9261d6976be6f8dff52
Jonathan-phan-804 <jonathan.phan804@gmail.com>
2022-10-31 12:33:24 -0700

    Added authorization token

--- Lib/Utils/upload_utils.dart
+++ Lib/Utils/upload_utils.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:song_snippet/Utils/API_Utils/http_header_strings.dart';
 
 Future<String> uploadSong(
     filename, url, songName, artistName, year, startTime, endTime) async {
   var request = http.MultipartRequest('POST', Uri.parse(url));
   request.files.add(await http.MultipartFile.fromPath('song', filename));
+  request.headers['authorization'] =
+      'TOKEN b03cdb0207d3769420b392838c980d23e431877e';
   request.fields['start'] = startTime;
   request.fields['end'] = endTime;
   request.fields['artist'] = artistName;
