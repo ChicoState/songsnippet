b6334ca248a57bdbfe3ee80574dffa6936add2c1
vmartinez40 <vmartinez40@mail.csuchico.edu>
2022-10-30 19:35:05 -0700

    Added swiping widgets front end but without integrations

---
+++ lib/model/profile.dart
@@ -0,0 +1,10 @@
+class Profile {
+  const Profile({
+    required this.songName,
+    required this.artist,
+    required this.imageAsset,
+  });
+  final String songName;
+  final String artist;
+  final String imageAsset;
+}
